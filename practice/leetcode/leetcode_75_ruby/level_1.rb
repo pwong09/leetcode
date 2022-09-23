7854dd41e3a2c423988874f0dcf7ae9ba74ff42d
pwong09 <pwong09@gmail.com>
2022-09-23 11:50:26 -0700

    Submitted pivot_index method

--- practice/leetcode/leetcode_75_ruby/level_1.rb
+++ practice/leetcode/leetcode_75_ruby/level_1.rb
@@ -13,3 +13,17 @@ end
 # refactor
 # total = 0
 # nums.map { |number| total += number }
+
+# @param {Integer[]} nums
+# @return {Integer}
+def pivot_index(nums)
+  left_sum = 0
+  sum = nums.reduce(:+)
+  nums.each_with_index do |num, i|
+    sum -= num
+    return i if left_sum == sum
+
+    left_sum += num
+  end
+  -1
+end
